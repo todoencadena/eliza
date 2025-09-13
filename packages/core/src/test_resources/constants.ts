a932b7da86feeae5f6f3a19fd7e902b33ad01893
Christopher Trimboli <cjft@trimboli.io>
2025-09-13 17:17:34 -0600

    remove more supa keys

--- packages/core/src/test_resources/constants.ts
+++ packages/core/src/test_resources/constants.ts
@@ -1,9 +1,8 @@
 import type { UUID } from '@elizaos/core';
 
 export const SERVER_URL = 'http://localhost:7998';
-export const SUPABASE_URL = 'https://pronvzrzfwsptkojvudd.supabase.co';
-export const SUPABASE_ANON_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InByb252enJ6ZndzcHRrb2p2dWRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY4NTYwNDcsImV4cCI6MjAyMjQzMjA0N30.I6_-XrqssUb2SWYg5DjsUqSodNS3_RPoET3-aPdqywM';
+export const SUPABASE_URL = process.env.SUPABASE_URL || '';
+export const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY || '';
 export const TEST_EMAIL = 'testuser123@example.com';
 export const TEST_PASSWORD = 'mock_password_123!@#';
 export const TEST_EMAIL_2 = 'testuser234@example.com';
